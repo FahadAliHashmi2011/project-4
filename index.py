a5e3ec2e9ee7c7a346820fb8fe92877c5e29a2f1
FahadAliHashmi2011 <faiezali2006@gmail>
2025-10-04 13:15:03 +0100

    s

--- index.py
+++ index.py
@@ -1,3 +1,9 @@
 x = int(input("please enter a number to calculate its power :"))
 x=(x ** x)
 print(x)
+
+
+#------------------------------------------------------------
+xy  = int(input("please enter a number"))
+bin(xy)
+print("the binary version of this",xy)
